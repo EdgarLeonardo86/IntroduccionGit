02a47bfdea6313effef821c232097d0c96caef88
EdgarLeonardo86 <edgar_leonar86@hotmail.com>
2021-06-16 19:50:16 +0100

    Creación de la clase Docente

---
+++ src/docentes/Docente.java
@@ -0,0 +1,17 @@
+package docentes;
+
+public class Docente {
+    private String nombre;
+
+    public Docente(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
